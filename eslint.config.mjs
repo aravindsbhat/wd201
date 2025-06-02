ef0d5250bb40914c86cbcd6d999bd7ead253efe9
Aravind S Bhat <aravindsbhat@gmail.com>
2025-06-02 06:25:10 +0000

    fixed lint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
+    ignores: ["**/migrations/**", "**/seeders/**", "**/models/**"],
+  },
+  {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
     extends: ["js/recommended"],
